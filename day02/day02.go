97f92133b32ca78a357bbf0a82b9a8536161d45c
FMinister <johannes.leyrer+git@googlemail.com>
2023-12-04 12:14:04 +0100

    add solution for day 2 part 2

--- day02/day02.go
+++ day02/day02.go
@@ -1,7 +1,6 @@
 package day02
 
 import (
-	"log"
 	"regexp"
 	"strconv"
 
@@ -48,11 +47,46 @@ func Day02_1() int {
 			}
 		}
 
-		log.Printf("Line %d: %v; %v; !shouldNotAdd: %t\n", i+1, colorArray, colorCounts, !shouldNotAdd)
-
 		if !shouldNotAdd {
 			sum = sum + i + 1
 		}
+	}
+
+	return sum
+}
+
+func Day02_2() int {
+	fileText := hf.OpenAndReadFile("./day02/input.txt")
+
+	re := regexp.MustCompile(`(\d+) (\w+)`)
+
+	sum := 0
+
+	for _, line := range fileText {
+		colorCounts := make(map[string]int)
+		matches := re.FindAllStringSubmatch(line, -1)
+
+		for _, match := range matches {
+			count := match[1]
+			color := match[2]
+
+			countInt, _ := strconv.Atoi(count)
+
+			existingCount, ok := colorCounts[color]
+			if !ok {
+				colorCounts[color] = countInt
+			} else if countInt > existingCount {
+				colorCounts[color] = countInt
+			}
+		}
+
+		colorArray := make([]int, 3)
+
+		colorArray[0] = colorCounts["red"]
+		colorArray[1] = colorCounts["green"]
+		colorArray[2] = colorCounts["blue"]
+
+		sum = sum + (colorArray[0] * colorArray[1] * colorArray[2])
 	}
 
 	return sum
--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	// sumDay1Part1 := d01.Day01_1()
 	// sumDay1Part2 := d01.Day01_2()
-	sumDay2Part1 := d02.Day02_1()
+	// sumDay2Part1 := d02.Day02_1()
+	sumDay2Part2 := d02.Day02_2()
 
-	log.Printf("Done: %d\n", sumDay2Part1)
+	log.Printf("Done: %d\n", sumDay2Part2)
 }
