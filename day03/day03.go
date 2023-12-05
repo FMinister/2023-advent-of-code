5c7cdabbb1b889359ab365f107f287c2fcbe12e0
FMinister <johannes.leyrer+git@googlemail.com>
2023-12-05 13:25:23 +0100

    add solution for day 3 part 1

--- day03/day03.go
+++ day03/day03.go
@@ -1,5 +1,61 @@
 package day03
 
+import (
+	"log"
+	"regexp"
+	"strconv"
+
+	hf "github.com/FMinister/2023-advent-of-code/helperfunctions"
+)
+
 func Day03_1() int {
-	return 0
+	fileText := hf.OpenAndReadFile("./day03/input.txt")
+
+	reSpecial := regexp.MustCompile(`[*/@\&=+#%$-]`)
+	reDigit := regexp.MustCompile(`\d{1,}`)
+
+	specials := make([][]int, len(fileText))
+
+	sum := 0
+
+	for i, line := range fileText {
+		matches := reSpecial.FindAllStringIndex(line, -1)
+
+		for _, match := range matches {
+			specials[i] = append(specials[i], match[0])
+		}
+	}
+
+	log.Printf("Specials: %v", specials)
+
+	for i, line := range fileText {
+		matches := reDigit.FindAllStringSubmatchIndex(line, -1)
+
+		for _, match := range matches {
+			if i > 0 {
+				for _, special := range specials[i-1] {
+					if match[0]-1 == special || match[0] == special || match[0]+1 == special || match[1] == special || match[1]-1 == special {
+						number, _ := strconv.Atoi(line[match[0]:match[1]])
+						sum = sum + number
+					}
+				}
+			}
+			if i < len(fileText)-1 {
+				for _, special := range specials[i+1] {
+					if match[0]-1 == special || match[0] == special || match[0]+1 == special || match[1] == special || match[1]-1 == special {
+						number, _ := strconv.Atoi(line[match[0]:match[1]])
+						sum = sum + number
+					}
+				}
+			}
+			for _, special := range specials[i] {
+				if match[0]-1 == special || match[1] == special {
+					number, _ := strconv.Atoi(line[match[0]:match[1]])
+					sum = sum + number
+				}
+			}
+		}
+	}
+
+	return sum
 }
--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"log"
 
 	// d01 "github.com/FMinister/2023-advent-of-code/day01"
-	d02 "github.com/FMinister/2023-advent-of-code/day02"
+	// d02 "github.com/FMinister/2023-advent-of-code/day02"
+	d03 "github.com/FMinister/2023-advent-of-code/day03"
 )
 
 func main() {
 	// sumDay1Part1 := d01.Day01_1()
 	// sumDay1Part2 := d01.Day01_2()
 	// sumDay2Part1 := d02.Day02_1()
-	sumDay2Part2 := d02.Day02_2()
+	// sumDay2Part2 := d02.Day02_2()
+	sumDay3Part1 := d03.Day03_1()
 
-	log.Printf("Done: %d\n", sumDay2Part2)
+	log.Printf("Done: %d\n", sumDay3Part1)
 }
