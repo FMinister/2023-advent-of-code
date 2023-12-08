c19d50387bd67485062bf30bde8056e1d753097a
FMinister <johannes.leyrer+git@googlemail.com>
2023-12-08 01:39:36 +0100

    update main

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 
 	// d01 "github.com/FMinister/2023-advent-of-code/day01"
 	// d02 "github.com/FMinister/2023-advent-of-code/day02"
-	d03 "github.com/FMinister/2023-advent-of-code/day03"
+	// d03 "github.com/FMinister/2023-advent-of-code/day03"
+	d04 "github.com/FMinister/2023-advent-of-code/day04"
 )
 
 func main() {
@@ -14,7 +15,8 @@ func main() {
 	// sumDay2Part1 := d02.Day02_1()
 	// sumDay2Part2 := d02.Day02_2()
 	// sumDay3Part1 := d03.Day03_1()
-	sumDay3Part2 := d03.Day03_2()
+	// sumDay3Part2 := d03.Day03_2()
+	sumDay4Part1 := d04.Day04_1()
 
-	log.Printf("Done: %d\n", sumDay3Part2)
+	log.Printf("Done: %d\n", sumDay4Part1)
 }
