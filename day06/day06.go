6bceee7d204fe3ea4bfeb0dee84b923f5aa06779
FMinister <johannes.leyrer+git@googlemail.com>
2023-12-10 19:09:23 +0100

    add base for day 6

---
+++ day06/day06.go
@@ -0,0 +1,15 @@
+package day06
+
+import (
+	"log"
+
+	hf "github.com/FMinister/2023-advent-of-code/helperfunctions"
+)
+
+func Day06_1() int {
+	fileText := hf.OpenAndReadFile("./day06/example.input.txt")
+
+	log.Printf("Done: %v\n", fileText)
+
+	return 0
+}
--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	// d02 "github.com/FMinister/2023-advent-of-code/day02"
 	// d03 "github.com/FMinister/2023-advent-of-code/day03"
 	// d04 "github.com/FMinister/2023-advent-of-code/day04"
-	d05 "github.com/FMinister/2023-advent-of-code/day05"
+	// d05 "github.com/FMinister/2023-advent-of-code/day05"
+	d06 "github.com/FMinister/2023-advent-of-code/day06"
 )
 
 func main() {
@@ -21,7 +22,8 @@ func main() {
 	// sumDay4Part2 := d04.Day04_2()
 	// sumDay5Part1 := d05.Day05_1()
 	// sumDay5Part2 := d05.Day05_2()
-	sumDay5Part2 := d05.Day05_2_goroutines()
+	// sumDay5Part2 := d05.Day05_2_goroutines()
+	sumDay6Part1 := d06.Day06_1()
 
-	log.Printf("Done: %d\n", sumDay5Part2)
+	log.Printf("Done: %d\n", sumDay6Part1)
 }
