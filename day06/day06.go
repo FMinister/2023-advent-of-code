33fd13c646d085cb28f55c95dcf250f15fe0b393
FMinister <johannes.leyrer+git@googlemail.com>
2023-12-11 01:07:55 +0100

    add solution for day 6 part 1

--- day06/day06.go
+++ day06/day06.go
@@ -1,15 +1,49 @@
 package day06
 
 import (
-	"log"
+	"regexp"
 
 	hf "github.com/FMinister/2023-advent-of-code/helperfunctions"
 )
 
 func Day06_1() int {
-	fileText := hf.OpenAndReadFile("./day06/example.input.txt")
+	fileText := hf.OpenAndReadFile("./day06/input.txt")
 
-	log.Printf("Done: %v\n", fileText)
+	time := []int{}
+	distance := []int{}
+	re := regexp.MustCompile(`(\d+)`)
 
-	return 0
+	for i := 0; i < len(fileText); i++ {
+		line := fileText[i]
+
+		timeOrDist := re.FindAllString(line, -1)
+
+		if len(time) == 0 {
+			time = hf.StringsToInts(timeOrDist)
+		} else {
+			distance = hf.StringsToInts(timeOrDist)
+		}
+	}
+
+	wins := 1
+
+	for i := 0; i < len(time); i++ {
+		possibleWins := 0
+		timeAvailable := time[i]
+		distanceToBeat := distance[i]
+
+		for j := 0; j < timeAvailable; j++ {
+			speed := j
+			timeCharging := j
+			timeSprinting := timeAvailable - timeCharging
+			distanceTravelled := timeSprinting * speed
+			if distanceTravelled > distanceToBeat {
+				possibleWins++
+			}
+		}
+
+		wins = wins * possibleWins
+	}
+
+	return wins
 }
